ecfb093b8a86f930b3c972047a2290e2741d7688
daxiami <daxiami888@qq.com>
2020-09-04 08:29:47 +0000

    初始化

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/test1', function () {
+    return "test1";
+});
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
